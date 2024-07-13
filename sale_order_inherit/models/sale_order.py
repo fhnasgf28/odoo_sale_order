cc8917718542762fd7243f73b8c2b6ba17d4e245
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-07-13 12:47:59 +0700

    [add] add compute field, not fix

--- sale_order_inherit/models/sale_order.py
+++ sale_order_inherit/models/sale_order.py
@@ -5,7 +5,6 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     is_booking = fields.Boolean(string='Is Booking', default=False)
-    qty = fields.Float(string='Quantity', default=1.0)
 
     @api.model
     def create(self, vals):
@@ -18,3 +17,24 @@ class SaleOrder(models.Model):
             vals['name'] = self.env['ir.sequence'].next_by_code(sequence_code) or _('New')
         res = super(SaleOrder, self).create(vals)
         return res
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    qty_booking = fields.Float(string='Quantity Booking', default=1.0)
+
+    @api.onchange('product_uom_qty')
+    def _onchange_qty_booking(self):
+        for line in self:
+            line.qty_booking = line.product_uom_qty
+
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        print('method ini di klik')
+        for line in self:
+            if line.order_id.is_booking and line.product_id:
+                line.price_unit = line.product_id.lst_price * 1.10
+            else:
+                line.price_unit = line.product_id.lst_price
+
